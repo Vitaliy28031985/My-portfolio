8cb73616044bb908e29cbfca2cc41b180bce24a2
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2023-05-22 20:10:47 +0300

    fix1958

--- src/js/db/projects.js
+++ src/js/db/projects.js
@@ -20,7 +20,7 @@ export const projects = [
     avatar: iceCream,
     title: "IceCream",
     description:
-      "Верстка веб-сайту для фірми яка займається виробництвом унікального морозива. Метою сайту є як залучення клієнтів так і продаж морозива онлайн.",
+      "Верстка веб-сайту для фірми яка займається виробництвом унікального морозива. Метою сайту є, як залучення клієнтів так і продаж морозива онлайн.",
     technologies: "HTML, CSS, SCSS Java Script, GitHub",
     linkWeb: "https://slavikidak.github.io/project-pink/",
     linkGit: "https://github.com/slavikidak/project-pink",
@@ -30,7 +30,7 @@ export const projects = [
     avatar: filmoteka,
     title: "Filmoteka",
     description:
-      "Створено веб-сайт для пошуку новинок кіно. Метою сайту є надання користувачеві можливості підбору фільму для перегляду. З допомогою веб-застосунку користувач може: прочитати коротку інформацію про фільм переглянути його трейлер та прийняти рішення чи варто цей фільм переглядати.",
+      "Створено веб-сайт для пошуку новинок кіно. Метою сайту, є надання користувачеві можливості підбору фільму для перегляду. З допомогою веб-застосунку користувач може: прочитати коротку інформацію про фільм переглянути його трейлер та прийняти рішення чи варто цей фільм переглядати.",
     technologies: "HTML, CSS, SCSS Java Script, GitHub, Webpack",
     linkWeb: "https://maksmaximusmaxim.github.io/-team-project/index.html",
     linkGit: "https://github.com/Maksmaximusmaxim/-team-project",
@@ -40,7 +40,7 @@ export const projects = [
     avatar: petly,
     title: "Petly",
     description:
-      "Веб-застосунок створений для власників домашніх тварин. З його допомогою можна придбати домашнього улюбленця. Якщо хтось знайшов загублену тваринку то він може подати оголошення про знахідку. Також з допомогою застосунку можна віддати в добрі руки домашню тваринку.",
+      "Веб-застосунок створений для власників домашніх тварин. З його допомогою можна придбати домашнього улюбленця. Якщо хтось знайшов загублену тваринку, то він може подати оголошення про знахідку. Також з допомогою застосунку можна віддати в добрі руки домашню тваринку.",
     technologies:
       "HTML, CSS, SASS , Java Script, React, Reduxtoolk, RTK Query, Netlify Service, Node.JS, Express, Mongoose, Cloudinary, Webpack, GitHub",
     linkWeb: "https://sk-care-pets.netlify.app/",
@@ -51,7 +51,7 @@ export const projects = [
     avatar: FinanceLedger,
     title: "Finance Ledger",
     description:
-      "Верстка веб-сайту для фірми яка займається наданням фінансових послуг. Метою сайту є залучення клієнтів.",
+      "Верстка веб-сайту для фірми яка займається наданням фінансових послуг. Метою сайту, є залучення клієнтів.",
     technologies: "HTML, CSS, SCSS Java Script, GitHub, Webpack",
     linkWeb: "https://ornate-tiramisu-a67827.netlify.app",
     linkGit: "https://github.com/Vitaliy28031985/finance-ledger",
