0a52d6d9a6a9c8c560673fdb8c12d1625af17ad4
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2023-06-02 11:04:42 +0300

    one videos

--- src/js/db/videos.js
+++ src/js/db/videos.js
@@ -4,6 +4,9 @@ export const videos = [
       link: "https://www.youtube.com/embed/A6P1-kr9GHQ?controls"
    },
    {
-      link: "https://www.youtube.com/embed/tnpX9AkP2Jc"
+      link: "https://www.youtube.com/embed/UWyPWQcarWI?start=2"
+   },
+   {
+      link: "https://www.youtube.com/embed/0lDPKLlWixY?start=2"
    }
 ]
